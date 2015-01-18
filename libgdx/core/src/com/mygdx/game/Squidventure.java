5d84524232a5dfc5085e79382274324d97e16c96
Andy <admazzola@gmail.com>
2015-01-17 21:16:52 -0500

    squid is swimming i a sea of blood

--- libgdx/core/src/com/mygdx/game/Squidventure.java
+++ libgdx/core/src/com/mygdx/game/Squidventure.java
@@ -4,24 +4,42 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 
 public class Squidventure extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Sprite sprite;
+	
+	boolean ready;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		
+		TextureAtlas atlas;
+		atlas = new TextureAtlas(Gdx.files.internal("spritesheets/playerpoop.atlas"));
+		 sprite = atlas.createSprite("p1_walk01");
+		 ready =true;
 	}
 
 	@Override
 	public void render () {
+		if(!ready)
+		{return;}
+		
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
+		sprite.draw(batch);
+		
 		batch.end();
 	}
 }
